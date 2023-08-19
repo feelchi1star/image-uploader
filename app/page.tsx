fd14497f6cb672a2a36a5a45daab990a92fb3921
Felix chinonso Emmanuel <Feelchi1star@gmail.com>
2023-08-19 17:43:09 +0100

    uploaded Drag and drop feature

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,22 @@ import React from "react";
 export default function Home() {
   const [file, setFile] = React.useState({});
   const [img, setImage] = React.useState<string | null>(null);
+  const [dragStyle, setDragStyle] = React.useState<boolean>(false);
+  const [iserror, setIsError] = React.useState<boolean>(false);
   const fileInput = React.useRef<HTMLInputElement>(null);
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    const data = e.currentTarget.file.files[0];
+    const formData = new FormData();
+    formData.append("picture", data);
   }
 
   async function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
     const availableFiles = e.target.files;
+    uploadFile(availableFiles);
+  }
+  function uploadFile(availableFiles: FileList | null) {
+    setIsError(false);
     if (availableFiles && availableFiles.length > 0) {
       const reader = new FileReader();
       reader.onload = (event) => {
@@ -24,11 +33,14 @@ export default function Home() {
       reader.onerror = (event) => {
         console.error("Error reading file:", event.target?.error);
       };
+      if (!availableFiles[0].type.startsWith("image/")) {
+        setIsError(true);
+        return;
+      }
       setFile(availableFiles[0]);
       return reader.readAsDataURL(availableFiles[0]);
     }
   }
-
   function handleButtonClick() {
     if (fileInput?.current) {
       fileInput.current.click();
@@ -36,45 +48,85 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div
-        className="h-[400px] p-4 rounded-2xl flex flex-col items-center 
-        gap-3 bg-white shadow-2xl
-      "
-      >
-        <h1 className="text-3xl">File Upload </h1>
-        <p>File should be Jpeg, Png, etc.</p>
-        <form
-          className="flex gap-3  flex-col justify-stretch"
-          onSubmit={handleSubmit}
+    <div
+      onDragOver={(e) => {
+        e.preventDefault();
+        setDragStyle(true);
+      }}
+      onDrop={(e) => {
+        e.preventDefault();
+        uploadFile(e.dataTransfer.files);
+        setDragStyle(false);
+      }}
+      onDragLeave={() => setDragStyle(false)}
+      className={`${
+        dragStyle ? "bg-yellow-50" : "ondrag_leave"
+      } min-h-screen  flex flex-col items-center justify-center`}
+    >
+      <div className="w-full max-w-md p-6">
+        <div
+          className={`p-4 rounded-2xl flex flex-col items-center 
+        gap-3  shadow-2xl
+      `}
         >
-          <div className="h-56 relative w-60">
-            {img && <Image src={img} fill alt="Picture" />}
-          </div>
-          <input
-            type="file"
-            onChange={handleFileChange}
-            name="file"
-            accept="image/*"
-            className="hidden"
-            ref={fileInput}
-          />
-          <div className="flex gap-3">
-            <button
-              className="p-3  bg-blue-600 text-white font-medium rounded-lg items-center hover:bg-blue-300 hover:text-yellow-100"
-              onClick={handleButtonClick}
-            >
-              Choose an Image
-            </button>
-            <button
-              className="p-3  bg-blue-600 text-white font-medium rounded-lg items-center hover:bg-blue-300 hover:text-yellow-100"
-              type="submit"
-            >
-              Upload
-            </button>
-          </div>
-        </form>
+          <h1 className="text-3xl">File Upload </h1>
+          <p
+            className={`transition-all  duration-150 ease-in-out ${
+              iserror ? "text-lg text-red-500" : "text-sm"
+            }`}
+          >
+            File should be Jpeg, Png, etc.
+          </p>
+          <form
+            className="flex gap-3  flex-col justify-stretch"
+            onSubmit={handleSubmit}
+          >
+            <div className="h-56 relative w-full ">
+              {img ? (
+                <Image
+                  src={img}
+                  className="h-56"
+                  width={400}
+                  height={300}
+                  alt="Picture"
+                />
+              ) : (
+                <Image
+                  src={require("../public/undraw_upload_image_re_svxx.svg")}
+                  className="h-56"
+                  width={400}
+                  height={300}
+                  alt="Picture"
+                />
+              )}
+            </div>
+            <input
+              type="file"
+              onChange={handleFileChange}
+              name="file"
+              accept="image/*"
+              className="hidden"
+              ref={fileInput}
+            />
+            <div className="flex gap-3">
+              <button
+                className="px-3 py-2 text-xs md:text-base bg-blue-600 text-white font-medium rounded-lg items-center hover:bg-blue-300 hover:text-yellow-100"
+                onClick={handleButtonClick}
+              >
+                Choose an Image
+              </button>
+              {img && (
+                <button
+                  className="px-3 py-2 text-xs md:text-base bg-blue-600 text-white font-medium rounded-lg items-center hover:bg-blue-300 hover:text-yellow-100"
+                  type="submit"
+                >
+                  Upload
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
